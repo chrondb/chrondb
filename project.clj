f700d044d640f977233bbcfc00c2254aec774fff
Avelino <t@avelino.xxx>
2019-12-15 11:21:21 -0300

    used clucie to reverse index search (full text search support) advantage of this within the JVM, I do not need to implement reverse index manually congrats
    Signed-off-by: Avelino <t@avelino.xxx>

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [org.clojure/data.json "0.2.7"]
                  [clj-jgit "1.0.0-beta3"]
+                 [clucie "0.4.2"]
                  [environ "1.1.0"]]
   :plugins [[lein-codox "0.10.7"]
             [environ/environ.lein "0.3.1"]]
