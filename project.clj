28997b113376c2502874f8841fed2ad3403301ff
Avelino <t@avelino.xxx>
2021-03-14 12:33:58 -0300

    upgrade clojure version to 1.10.2
    Signed-off-by: Avelino <t@avelino.xxx>

--- project.clj
+++ project.clj
@@ -3,13 +3,14 @@
   :url "https://github.com/avelino/chrondb"
   :license {:name "MIT"
             :url "https://github.com/avelino/chrondb/blob/master/LICENSE"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
+  :dependencies [[org.clojure/clojure "1.10.2"]
                  [org.clojure/data.json "0.2.7"]
                  [clj-jgit "1.0.1" :exclusions [org.eclipse.jgit/org.eclipse.jgit.gpg.bc]]
                  [clucie "0.4.2"]
                  [environ "1.1.0"]]
   :plugins [[lein-codox "0.10.7"]
-            [environ/environ.lein "0.3.1"]]
+            [environ/environ.lein "0.3.1"]
+            [lein-marginalia "0.9.1"]]
   :main ^:skip-aot chrondb.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
