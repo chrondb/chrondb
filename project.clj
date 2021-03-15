a6757f3c2ee9bec15bff67b3a3d0f29433b55d19
Avelino <t@avelino.xxx>
2021-03-15 11:58:56 -0300

    use gzip to data struct (json) compress/decompress all content (json) will be lightly compressed using gzip
    Signed-off-by: Avelino <t@avelino.xxx>

--- project.clj
+++ project.clj
@@ -4,12 +4,13 @@
   :license {:name "MIT"
             :url "https://github.com/avelino/chrondb/blob/master/LICENSE"}
   :dependencies [[org.clojure/clojure "1.10.2"]
-                 [org.clojure/data.json "0.2.7"]
+                 [org.clojure/data.json "1.1.0"]
                  [clj-jgit "1.0.1" :exclusions [org.eclipse.jgit/org.eclipse.jgit.gpg.bc]]
                  [clucie "0.4.2"]
-                 [environ "1.1.0"]]
+                 [environ "1.2.0"]
+                 [middlesphere/clj-compress "0.1.0"]]
   :plugins [[lein-codox "0.10.7"]
-            [environ/environ.lein "0.3.1"]
+            [lein-environ "1.2.0"]
             [lein-marginalia "0.9.1"]]
   :main ^:skip-aot chrondb.core
   :target-path "target/%s"
