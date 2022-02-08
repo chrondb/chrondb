f50688742aff3058b1baf095b8bfc004182ec8d6
Avelino <avelinorun@gmail.com>
2022-02-08 19:20:18 -0300

    doc string: describe functionalities of functions
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/chrondb/func.clj
+++ src/chrondb/func.clj
@@ -1,28 +1,30 @@
 (ns chrondb.func
-  (:require [clojure.java.io :as io]
+  (:require [clj-compress.core :as c]
+            [clj-jgit.porcelain :as jgit]
             [clojure.data.json :as json]
-            [clj-compress.core :as c]
-            [clj-jgit.porcelain :as jgit])
-  (:import (org.eclipse.jgit.api Git)
-           (java.io ByteArrayOutputStream)))
+            [clojure.java.io :as io])
+  (:import (java.io ByteArrayOutputStream)
+           (org.eclipse.jgit.api Git)))
 
 (def file-ext ".cdb")
 (def back-to-repo "/../")
 (def compressor-type "gz")
 
 (defn path->repo
+  "transform path in git repository, if it doesn't exist it will be created"
   [path]
   (if (.isDirectory (io/file path))
     (jgit/load-repo path)
     (jgit/git-init :dir path)))
 
 (defn save
+  "save (insert/update) information in the repository"
   [^Git repo key value
-   & {:keys [msg commiter no-verify? sign?]
-      :or   {msg        "saving content"
-             commiter   {:name "chrondb-anonymous"}
-             no-verify? true
-             sign?      false}}]
+   &{:keys [msg commiter no-verify? sign?]
+     :or   {msg        "saving content"
+            commiter   {:name "chrondb-anonymous"}
+            no-verify? true
+            sign?      false}}]
   (let [repo-git-dir (-> repo .getRepository .getDirectory .toString)
         repo-dir (str repo-git-dir back-to-repo)
         data-filename (str key file-ext)
@@ -35,7 +37,9 @@
                 :sign? sign?
                 :commiter commiter)))
 
-(defn find-by-key [^Git repo key]
+(defn find-by-key
+  "find by key registered in the git repository"
+  [^Git repo key]
   (let [repo-git-dir (-> repo .getRepository .getDirectory .toString)
         repo-dir (str repo-git-dir back-to-repo)
         data-filename (str key file-ext)
--- src/chrondb/gzip/convert.clj
+++ src/chrondb/gzip/convert.clj
@@ -4,6 +4,7 @@
            [java.util.zip GZIPInputStream GZIPOutputStream]))
 
 (defn str->gzipped-bytes
+  "gzip string compression"
   ^ByteArrayOutputStream [^String str]
   (with-open [out (ByteArrayOutputStream.)
               gzip (GZIPOutputStream. out)]
@@ -12,9 +13,10 @@
     (.toByteArray out)))
 
 (defn gzipped-input-stream->str
+  "decompress gzip to string"
   ^String
-  [^ByteArrayInputStream input-stream & {:keys [^String encoding]
-                                         :or   {encoding "UTF-8"}}]
+  [^ByteArrayInputStream input-stream &{:keys [^String encoding]
+                                        :or   {encoding "UTF-8"}}]
   (with-open [out (ByteArrayOutputStream.)]
     (io/copy (GZIPInputStream. input-stream) out)
     (.close input-stream)
