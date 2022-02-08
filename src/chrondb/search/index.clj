2cbd38b21505d604900294c879421c91fc3d62ff
Avelino <avelinorun@gmail.com>
2022-02-08 19:19:15 -0300

    lucine: add support for persistent search index
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- src/chrondb/search/index.clj
+++ src/chrondb/search/index.clj
@@ -2,5 +2,14 @@
   (:require [clucie.analysis :as analysis]
             [clucie.store :as store]))
 
-(def analyzer (analysis/standard-analyzer))
-(def store (store/memory-store)) ; or (store/disk-store "path/to/store")
+(def analyzer
+  "lucene analyzer"
+  (analysis/standard-analyzer))
+
+(defn store
+  "lucene store (memory or disk), if the type is other than `memory` it must be the path that will be saved"
+  [&{:keys [type]
+     :or {type "memory"}}]
+  (if (identical? type "memory")
+    (store/memory-store)
+    (store/disk-store type)))
