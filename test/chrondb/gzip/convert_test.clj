a81f21184481a9b5b714c6574994e43b75c3df18
Avelino <avelinorun@gmail.com>
2022-02-14 18:20:34 -0300

    test/gzip: formatter
    Signed-off-by: Avelino <avelinorun@gmail.com>

--- test/chrondb/gzip/convert_test.clj
+++ test/chrondb/gzip/convert_test.clj
@@ -1,23 +1,23 @@
 (ns chrondb.gzip.convert-test
   (:require [clojure.test :refer [deftest is testing]]
-            [chrondb.gzip.convert :refer [str->gzipped-bytes gzipped-input-stream->str]]))
+            [chrondb.gzip.convert :as convert]))
 
 (def test-str "π3.14")
-(def gzipped (str->gzipped-bytes test-str))
+(def gzipped (convert/str->gzipped-bytes test-str))
 
 (deftest str-to-gzip
   (testing "convert string -> gzip"
     (is (= (type (byte-array []))
-           (type (str->gzipped-bytes test-str))))
-    (is (bytes? (str->gzipped-bytes test-str)))))
+           (type (convert/str->gzipped-bytes test-str))))
+    (is (bytes? (convert/str->gzipped-bytes test-str)))))
 
 (deftest gzip-to-str
   (testing "convert gzip -> string"
     (is (= test-str
-           (gzipped-input-stream->str
-             (java.io.ByteArrayInputStream. gzipped)))))
+           (convert/gzipped-input-stream->str
+            (java.io.ByteArrayInputStream. gzipped)))))
   (testing "check encode UTF-16"
     (is (= "쾀㌮ㄴ"
-           (gzipped-input-stream->str
-             (java.io.ByteArrayInputStream. gzipped)
-             :encoding "UTF-16")))))
+           (convert/gzipped-input-stream->str
+            (java.io.ByteArrayInputStream. gzipped)
+            :encoding "UTF-16")))))
