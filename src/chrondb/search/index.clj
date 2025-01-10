24b5121bdba73034a027c84619eba2bf4551ac2f
Avelino <31996+avelino@users.noreply.github.com>
2025-01-10 07:39:42 -0300

    refactor: new package architecture
    - Removed search/index.clj (moved to index/protocol.clj)
    - Organized .gitignore file
    - Updated dependencies in deps.edn
    
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/chrondb/search/index.clj
+++
@@ -1,9 +0,0 @@
-(ns chrondb.search.index
-  (:require [clucie.store :as store]
-            [clucie.analysis :as analysis]))
-
-(def analyzer (analysis/standard-analyzer))
-
-(defn store
-  [& _]
-  (store/memory-store))
--- test/chrondb/api/server_test.clj
+++ test/chrondb/api/server_test.clj
@@ -1,8 +1,17 @@
-(ns chrondb.api.server-test
+(ns chrondb.api.server_test
   (:require [clojure.test :refer [deftest is testing]]
             [chrondb.api.server :as server]
             [chrondb.storage.memory :as memory]
-            [chrondb.index.lucene :as lucene]))
+            [chrondb.index.lucene :as lucene]
+            [chrondb.test-helpers :as helpers]
+            [clojure.data.json :as json]
+            [ring.mock.request :as mock]))
+
+(defn parse-json-body [response]
+  (when-let [body (:body response)]
+    (if (string? body)
+      (json/read-str body :key-fn keyword)
+      body)))
 
 (deftest test-server
   (testing "Server creation"
@@ -11,4 +20,49 @@
           app (server/create-app storage index)]
       (is (fn? app))
       (.close storage)
-      (.close index)))) 
+      (.close index)
+      (helpers/delete-directory "test-index"))))
+
+(deftest test-json-middleware
+  (testing "JSON middleware"
+    (helpers/with-test-data [storage index]
+      (let [app (server/create-app storage index)
+            doc {:id "test:1" :name "Test Doc" :value 42}]
+
+        (testing "Valid JSON request"
+          (let [request (-> (mock/request :post "/api/v1/save")
+                            (assoc :body (json/write-str doc))
+                            (assoc-in [:headers "content-type"] "application/json"))
+                response (app request)]
+            (is (= 200 (:status response)))
+            (is (= doc (parse-json-body response)))))
+
+        (testing "Invalid JSON request"
+          (let [request (-> (mock/request :post "/api/v1/save")
+                            (assoc :body "{invalid json}")
+                            (assoc-in [:headers "content-type"] "application/json"))
+                response (app request)]
+            (is (= 400 (:status response)))
+            (is (:error (parse-json-body response)))))
+
+        (testing "Empty body request"
+          (let [request (mock/request :get "/api/v1/get/test:1")
+                response (app request)]
+            (is (= 404 (:status response)))))
+
+        (testing "JSON response"
+          (let [request (mock/request :get "/")
+                response (app request)]
+            (is (= "application/json" (get-in response [:headers "Content-Type"])))
+            (is (string? (:body response)))))))))
+
+(deftest test-server-startup
+  (testing "Server startup"
+    (let [storage (memory/create-memory-storage)
+          index (lucene/create-lucene-index "test-index")
+          server (server/start-server storage index 0)]
+      (is (not (nil? server)))
+      (.stop server)
+      (.close storage)
+      (.close index)
+      (helpers/delete-directory "test-index")))) 
