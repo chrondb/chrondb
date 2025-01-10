e608111fdb401336134d6d43231163fecc7173d4
Avelino <31996+avelino@users.noreply.github.com>
2025-01-10 08:50:13 -0300

    wip: broken - will continue on another laptop
    Signed-off-by: Avelino <31996+avelino@users.noreply.github.com>

--- src/chrondb/storage/git.clj
+++ src/chrondb/storage/git.clj
@@ -2,7 +2,8 @@
   (:require [chrondb.storage.protocol :as protocol]
             [clojure.java.io :as io]
             [clojure.data.json :as json])
-  (:import [org.eclipse.jgit.api Git]))
+  (:import [org.eclipse.jgit.api Git]
+           [org.eclipse.jgit.transport URIish RefSpec]))
 
 (defn ensure-directory
   "Creates a directory if it doesn't exist"
@@ -15,20 +16,94 @@
         true))))
 
 (defn create-repository
-  "Creates a new Git repository at the specified path"
+  "Creates a new bare Git repository at the specified path"
   [path]
   (ensure-directory path)
-  (let [git (-> (Git/init)
-                (.setDirectory (io/file path))
-                (.call))
-        repo (.getRepository git)]
-    (-> git
-        (.commit)
-        (.setMessage "Initial empty commit")
-        (.setAllowEmpty true)
-        (.setSign false)
-        (.call))
-    repo))
+  (let [temp-path (str path "-temp")
+        temp-repo (-> (Git/init)
+                      (.setDirectory (io/file temp-path))
+                      (.call))]
+    (try
+      ;; Create initial commit in temp repo
+      (-> temp-repo
+          (.commit)
+          (.setMessage "Initial empty commit")
+          (.setAllowEmpty true)
+          (.setSign false)
+          (.call))
+      ;; Create main branch
+      (-> temp-repo
+          (.branchCreate)
+          (.setName "main")
+          (.setForce true)
+          (.call))
+      ;; Create bare repo
+      (let [bare-repo (-> (Git/init)
+                          (.setDirectory (io/file path))
+                          (.setBare true)
+                          (.call)
+                          (.getRepository))]
+        ;; Add bare repo as remote
+        (-> temp-repo
+            (.remoteAdd)
+            (.setName "origin")
+            (.setUri (-> (URIish.)
+                         (.setScheme "file")
+                         (.setPath (.getAbsolutePath (.getDirectory bare-repo)))))
+            (.call))
+        ;; Push to bare repo with force
+        (-> temp-repo
+            (.push)
+            (.setRemote "origin")
+            (.setRefSpecs (java.util.Collections/singletonList
+                           (org.eclipse.jgit.transport.RefSpec. "+refs/heads/main:refs/heads/main")))
+            (.setForce true)
+            (.call))
+        bare-repo)
+      (finally
+        (.close temp-repo)
+        (doseq [file (reverse (file-seq (io/file temp-path)))]
+          (.delete file))))))
+
+(defn- with-temp-clone [repository f]
+  (let [temp-path (str (System/getProperty "java.io.tmpdir") "/chrondb-" (System/currentTimeMillis))
+        temp-repo (-> (Git/init)
+                      (.setDirectory (io/file temp-path))
+                      (.call))]
+    (try
+      ;; Add bare repo as remote
+      (-> temp-repo
+          (.remoteAdd)
+          (.setName "origin")
+          (.setUri (URIish. (str "file://" (.getAbsolutePath (.getDirectory repository)))))
+          (.call))
+      ;; Fetch from bare repo
+      (-> temp-repo
+          (.fetch)
+          (.setRemote "origin")
+          (.call))
+      ;; Create and checkout main branch tracking origin/main
+      (-> temp-repo
+          (.checkout)
+          (.setName "main")
+          (.setCreateBranch true)
+          (.setStartPoint "origin/main")
+          (.setUpstreamMode org.eclipse.jgit.api.CreateBranchCommand$SetupUpstreamMode/TRACK)
+          (.call))
+      ;; Execute function and push changes
+      (let [result (f temp-repo)]
+        (-> temp-repo
+            (.push)
+            (.setRemote "origin")
+            (.setRefSpecs (java.util.Collections/singletonList
+                           (org.eclipse.jgit.transport.RefSpec. "+refs/heads/main:refs/heads/main")))
+            (.setForce true)
+            (.call))
+        result)
+      (finally
+        (.close temp-repo)
+        (doseq [file (reverse (file-seq (io/file temp-path)))]
+          (.delete file))))))
 
 (defrecord GitStorage [repository data-dir]
   protocol/Storage
@@ -38,51 +113,57 @@
       (throw (Exception. "Document cannot be nil")))
     (when-not repository
       (throw (Exception. "Repository is closed")))
-    (let [git (Git. repository)
-          doc-path (str data-dir "/" (:id document) ".json")
-          doc-file (io/file doc-path)]
-      (ensure-directory (.getParentFile doc-file))
-      (spit doc-path (json/write-str document))
-      (-> git
-          (.add)
-          (.addFilepattern (str "data/" (:id document) ".json"))
-          (.call))
-      (-> git
-          (.commit)
-          (.setMessage "Save document")
-          (.setSign false)
-          (.call))
-      document))
+    (with-temp-clone repository
+      (fn [git]
+        (let [doc-path (str data-dir "/" (:id document) ".json")
+              doc-file (io/file (.getDirectory (.getRepository git)) doc-path)]
+          (ensure-directory (.getParentFile doc-file))
+          (spit doc-file (json/write-str document))
+          (-> git
+              (.add)
+              (.addFilepattern doc-path)
+              (.call))
+          (-> git
+              (.commit)
+              (.setMessage "Save document")
+              (.setSign false)
+              (.call))
+          document))))
 
   (get-document [_ id]
     (when repository
       (try
-        (let [doc-path (str data-dir "/" id ".json")]
-          (when (.exists (io/file doc-path))
-            (json/read-str (slurp doc-path) :key-fn keyword)))
+        (with-temp-clone repository
+          (fn [git]
+            (let [doc-path (str data-dir "/" id ".json")
+                  doc-file (io/file (.getDirectory (.getRepository git)) doc-path)]
+              (when (.exists doc-file)
+                (json/read-str (slurp doc-file) :key-fn keyword)))))
         (catch Exception _
           nil))))
 
   (delete-document [_ id]
-    (when repository
+    (if-not repository
+      false
       (try
-        (let [git (Git. repository)
-              doc-path (str data-dir "/" id ".json")
-              file (io/file doc-path)]
-          (if (.exists file)
-            (do
-              (.delete file)
-              (-> git
-                  (.rm)
-                  (.addFilepattern (str "data/" id ".json"))
-                  (.call))
-              (-> git
-                  (.commit)
-                  (.setMessage "Delete document")
-                  (.setSign false)
-                  (.call))
-              true)
-            false))
+        (with-temp-clone repository
+          (fn [git]
+            (let [doc-path (str data-dir "/" id ".json")
+                  doc-file (io/file (.getDirectory (.getRepository git)) doc-path)]
+              (if (.exists doc-file)
+                (do
+                  (.delete doc-file)
+                  (-> git
+                      (.rm)
+                      (.addFilepattern doc-path)
+                      (.call))
+                  (-> git
+                      (.commit)
+                      (.setMessage "Delete document")
+                      (.setSign false)
+                      (.call))
+                  true)
+                false))))
         (catch Exception _
           false))))
 
@@ -94,7 +175,7 @@
 (defn create-git-storage
   "Creates a new Git storage instance"
   [path]
-  (let [repo (create-repository path)
-        data-dir (str path "/data")]
-    (ensure-directory data-dir)
-    (->GitStorage repo data-dir))) 
+  (let [data-dir "data"]
+    (ensure-directory (str path "/" data-dir))
+    (let [repo (create-repository path)]
+      (->GitStorage repo data-dir)))) 
